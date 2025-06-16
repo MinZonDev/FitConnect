6e1216ec503fbecb6e59144977f4cef9bcbd74c6
MinZon <trongpro0307@gmail.com>
2025-06-17 00:01:40 +0700

    first commit

--- src/main/java/com/fitconnect/FitConnectApplication.java
+++ src/main/java/com/fitconnect/FitConnectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FitConnectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FitConnectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FitConnectApplication.class, args);
+    }
 
 }
